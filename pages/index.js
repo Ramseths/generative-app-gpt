e67d36154597911149455ea541084f6b0dae60e3
Ramseths <juan.ramseths@gmail.com>
2023-03-12 12:09:08 -0600

    feat: add auth routes, link into index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return <div> 
     <h1>Hola</h1>
+    <div>
+      <Link href="/api/auth/login"> Iniciar Sesión</Link>
+    </div>
   </div>;
 }
