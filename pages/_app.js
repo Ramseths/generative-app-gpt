15354cd5bcf082736a5c63cb921507bec1ee7f4a
Ramseths <juan.ramseths@gmail.com>
2023-03-12 15:58:12 -0600

    feat: create getLayout, _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import '../styles/globals.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 function MyApp({ Component, pageProps }) {
-  return <UserProvider><Component {...pageProps} /></UserProvider>;
+  const getLayout = Component.getLayout || ((page) => page);
+  return <UserProvider>{getLayout(<Component {...pageProps} />, pageProps)}</UserProvider>;
 }
 
 export default MyApp
