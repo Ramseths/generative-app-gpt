d54bc80a1e79a5f1cd65a4c5b171991ba72131ed
Ramseths <juan.ramseths@gmail.com>
2023-03-12 15:19:27 -0600

    feat: add user picture change: config.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 export default function Home() {
-  return <div> 
-    <h1>Hola</h1>
+
+  const {user} = useUser();
+  console.log("Info: ", user);
+
+  return (<div> 
+    <h1>Página de Inicio</h1>
     <div>
-      <Link href="/api/auth/login"> Iniciar Sesión</Link>
+      {!!user ? 
+      (<>
+        <div>
+          <Image
+            src={user.picture}
+            height={50}
+            width={50}
+          />
+        </div>
+        <Link href="/api/auth/logout">Salir</Link>
+      </>) :
+        (<Link href="/api/auth/login">Iniciar Sesión</Link>)
+      }
     </div>
-  </div>;
+  </div>
+  );
 }
