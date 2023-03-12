c39058dfb381b081a6a84492660dc88c084697b0
Ramseths <juan.ramseths@gmail.com>
2023-03-12 15:24:39 -0600

    change: add props feat: add required auth (idpost, createpost)

--- pages/post/[idpost].js
+++ pages/post/[idpost].js
@@ -1,5 +1,15 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
 export default function CreatePost() {
     return <div> 
       <h1>Pagina de identificador</h1>
     </div>;
   }
+
+  export const getServerSideProps = withPageAuthRequired(() => {
+    return {
+      props: {
+        
+      }
+    };
+  });
--- pages/post/create.js
+++ pages/post/create.js
@@ -1,5 +1,15 @@
-export default function CreatePost() {
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+export default function CreatePost(props) {
     return <div> 
       <h1>Crear nueva entrada</h1>
     </div>;
   }
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {
+      
+    }
+  };
+});
