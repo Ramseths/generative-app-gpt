6771e767fee0ce61e95e993cb3c9c083d5f0ce02
Ramseths <juan.ramseths@gmail.com>
2023-03-18 18:14:26 -0600

    feat: add styles from tailwind css

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,18 @@
 export const AppLayout = ({children}) => {
     return (
-        <div>
-            Layout
+        <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+            <div className="flex flex-col text-white overflow-hidden">
+                <div className="bg-gray-700">
+                    <div>Logo</div>
+                    <div>Salida</div>
+                </div>
+                <div className="flex-1 overflow-auto bg-gray-600">
+                    lista
+                </div>
+                <div className="bg-gray-700">
+                    salir
+                </div>
+            </div>
             <div>{children}</div>
         </div>
     );
