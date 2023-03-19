34cfcf64e8106aaf361558f4bb36589e68ed71eb
Ramseths <juan.ramseths@gmail.com>
2023-03-19 15:42:58 -0600

    add form (keys, topic)

--- pages/post/create.js
+++ pages/post/create.js
@@ -1,20 +1,42 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useState } from "react";
 import { AppLayout } from "../../components/AppLayout";
 
 export default function CreatePost(props) {
 
-    const handleClick = async () => {
+    const [content, setContent] = useState("");
+    const [topic, setTopic] = useState("");
+    const [keys, setKeys] = useState("");
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
       const res = fetch(`/api/generator`, {
-        method: "POST"
+        method: "POST",
+        headers: {
+          'content-type':'application/json'
+        },
+        body: JSON.stringify({topic, keys}),
       });
 
       const json = await (await res).json();
       console.log(json);
+      setContent(json.post.content);
     }
 
     return <div> 
-      <h1>Crear nueva entrada</h1>
-      <button className="btn" onClick={handleClick}>Generar</button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Genera un artículo sobre:</label>
+          <textarea className="resize-none border border-gray-500 w-full block my-3 px-5 py-2 rounded-sm" value={topic} onChange={e => setTopic(e.target.value)}></textarea>
+        </div>
+        <div>
+        <label>Palabras claves:</label>
+          <textarea className="resize-none border border-gray-500 w-full block my-3 px-5 py-2 rounded-sm" value={keys} onChange={e => setKeys(e.target.value)}></textarea>
+        </div>
+        <button className="btn" type="submit">Generar</button>
+      </form>
+      
+      <div className="max-w-screen-sm p-9" dangerouslySetInnerHTML={{__html: content}}></div>
     </div>;
   }
 
