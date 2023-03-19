11a2884501b1aa720e833bd30ebca000aaaf6e97
Ramseths <juan.ramseths@gmail.com>
2023-03-18 18:54:10 -0600

    change: remove content from index.js feat: add flex style

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,16 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+import {useUser} from "@auth0/nextjs-auth0/client";
+
 export const AppLayout = ({children}) => {
+
+    const {user} = useUser();
+
     return (
         <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
             <div className="flex flex-col text-white overflow-hidden">
-                <div className="bg-gray-700">
-                    <div>Logo</div>
-                    <div>Salida</div>
+                <div className="bg-gray-700 flex items-center gap-2 border-b border-b-white/50 h-20 px-2">
+                    {!!user ? 
+                    (<>
+                        <div className="min-w-[50px]">
+                        <Image
+                            src={user.picture}
+                            height={50}
+                            width={50}
+                            className="rounded-full"
+                        />
+                        </div>
+                        <div className="flex-1">
+                            <div className="font-bold">{user.name}</div>
+                            <Link href="/api/auth/logout" className="text-sm">Cerrar Sesión</Link>
+                        </div>
+                        
+                    </>) :
+                        (<Link href="/api/auth/login">Iniciar Sesión</Link>)
+                    }
                 </div>
+                
                 <div className="flex-1 overflow-auto bg-gray-600">
                     lista
                 </div>
                 <div className="bg-gray-700">
-                    salir
+                    <div>Logo</div>
+                    <div>Salida</div>
                 </div>
             </div>
             <div>{children}</div>
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,8 @@
-import Link from "next/link";
-import { useUser } from "@auth0/nextjs-auth0/client";
-import Image from "next/image";
-
 export default function Home() {
-
-  const {user} = useUser();
-  console.log("Info: ", user);
-
   return (<div> 
     <h1>Página de Inicio</h1>
     <div>
-      {!!user ? 
-      (<>
-        <div>
-          <Image
-            src={user.picture}
-            height={50}
-            width={50}
-          />
-        </div>
-        <Link href="/api/auth/logout">Salir</Link>
-      </>) :
-        (<Link href="/api/auth/login">Iniciar Sesión</Link>)
-      }
+      
     </div>
   </div>
   );
