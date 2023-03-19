bc9697086b839995338675f33feeb5a487b5c0da
Ramseths <juan.ramseths@gmail.com>
2023-03-19 14:58:09 -0600

    feat: add tokens, meta-description

--- pages/api/generator.js
+++ pages/api/generator.js
@@ -7,13 +7,24 @@ export default async function handler(req, res) {
     });
     const openai = new OpenAIApi(config);
 
+    const topic = "Top algortimos de machine learning";
+    const keys = "supervisado, no supervisado, por refuerzo";
+
     const response = await openai.createCompletion({
         model: 'text-davinci-003',
         temperature: 0,
         max_tokens: 3600,
-        prompt: "Escribe acerca de la vida humana"
+        prompt: `Escriba una entrada de blog larga sobre ${topic}, que se dirige a las siguientes palabras clave separadas por comas: ${keys}.
+        El contenido debe ser formateado en SEO-frendly HTML.
+        La respuesta debe incluir un titulo HTML apropiado y contenido meta de la descripción.
+        El formato de regreso debe ser un stringified JSON en el siguiente formato:
+        {
+            "content": Contenido del blog aquí
+            "title": Titulo aquí
+            "metaDescription": La meta descripción va aquí
+        }`,
     });
 
     console.log('Response', response);
-    res.status(200).json({ post: response.data.choices });
+    res.status(200).json({ post: JSON.parse(response.data.choices[0]?.text) });
 }
