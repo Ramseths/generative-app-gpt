486c29ee024c8dc0c7c2958d22816736a56beedc
Ramseths <juan.ramseths@gmail.com>
2023-03-27 18:11:23 -0600

    feat: add mongo db connect

--- pages/api/generator.js
+++ pages/api/generator.js
@@ -1,6 +1,22 @@
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 import { Configuration, OpenAIApi  } from "openai";
+import clientPromise from '../../lib/mongodb';
 
-export default async function handler(req, res) {
+export default withApiAuthRequired (async function handler(req, res) {
+    const {user} = await getSession(req, res);
+    const client = await clientPromise;
+    const db = client.db("GenerativeContent");
+
+    await db.collection("users").insertOne(
+        {
+            auth0id: user.sub,
+        }
+    );
+
+
+    const userProfile = await db.collection("users").findOne({
+        auth0id: user.sub
+    });
 
     const config = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
@@ -13,9 +29,8 @@ export default async function handler(req, res) {
         model: 'text-davinci-003',
         temperature: 0,
         max_tokens: 3600,
-        prompt: `Escriba una entrada de blog larga sobre un tutorial de ${topic}, que se dirige a las siguientes palabras clave separadas por comas: ${keys}.
-        El contenido debe ser formateado en HTML y también el HTML debe tener fragmentos de código.
-        La respuesta debe incluir un titulo HTML apropiado y contenido meta de la descripción.
+        prompt: `Escriba una entrada de blog larga sobre ${topic}, que se dirige a las siguientes palabras clave separadas por comas: ${keys}.
+        El contenido debe ser formateado en HTML SEO Frendly
         El formato de regreso debe ser un stringified JSON en el siguiente formato:
         {
             "content": Contenido del blog aquí
@@ -24,6 +39,18 @@ export default async function handler(req, res) {
         }`,
     });
 
+    const parsed = JSON.parse(response.data.choices[0]?.text.split("\n").join(""));
+
+    const post = await db.collection("content").insertOne({
+        content: parsed?.content,
+        title: parsed?.title,
+        metaDescription: parsed?.metaDescription,
+        topic,
+        keywords: keys,
+        userId: userProfile._id,
+        date: new Date(),
+    });
+
     console.log('Response', response);
     res.status(200).json({ post: JSON.parse(response.data.choices[0]?.text.split("\n").join("")) });
-}
+});
